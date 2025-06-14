27d5a46bf8084b65b379b020c19e750216b7d04c
yashpunmiya <yyash7379@gmail.com>
2025-06-14 15:04:58 +0530

    lint ts

--- solsmile/next.config.ts
+++ solsmile/next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-module.exports = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
+
 export default nextConfig;
